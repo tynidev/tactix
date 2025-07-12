ebb78cdbef12e82b015bf3ad914d0ed9693d3b5d
tynidev <nichols.tyler@gmail.com>
2025-07-12 15:32:26 -0600

    Update favicon and modify Vite configuration for asset inclusion

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     react(),
     VitePWA({
       registerType: 'autoUpdate',
-      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.svg'],
+      includeAssets: ['favicon.png', 'tactix-logo.png'],
       manifest: {
         name: 'Tactix HUD - Interactive Video Analysis',
         short_name: 'Tactix HUD',
